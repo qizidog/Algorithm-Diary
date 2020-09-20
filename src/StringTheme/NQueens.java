af9c89a007ff2850d25d19fbb9f10f9044f1d096
qizidog <451559450q@gmail.com>
2020-09-20 19:46:11 +0800

    2020.9.20 NQueens 拖了两天才来写这道题，被选题折磨惨了

--- src/StringTheme/NQueens.java
+++ src/StringTheme/NQueens.java
@@ -76,43 +76,81 @@ public class NQueens {
         return board;
     }
 
+    // 测试
+    public static void main(String[] args) {
+        NQueens nQueens = new NQueens();
+        List<List<String>> lists1 = nQueens.solveNQueens(5);
+        System.out.println(lists1);
+        System.out.println(lists1.size());
 
-    // 自己实现NQueens回溯算法
+
+        List<List<String>> lists2 = nQueens.solveNQueens2(5);
+        System.out.println(lists2);
+        System.out.println(lists2.size());
+    }
+
+    /*=====================================自己实现NQueens回溯算法===============================================*/
+
     public List<List<String>> solveNQueens2(int n) {
-        List<int[]> solutions = new ArrayList<int[]>();
+        List<List<String>> solutions = new ArrayList<List<String>>();
+        // List<int[]> solutions = new ArrayList<int[]>();
+        backtrack2(n, 0, new int[n], solutions);
+        return solutions;
+    }
 
+    /**
+        @param n: 一共有多少行
+        @param row: 当前是第几行，0表示第一行
+        @param solution: 前面几行的布置情况
+        @param solutions: 可行解
+     */
+    public void backtrack2(int n, int row, int[] solution, List<List<String>> solutions){
+        // 如果之前的n行都构建成功了
+        if (row==n){
+            solutions.add(getSolutionBoard(solution));
+            // solutions.add(solution);
+        }
 
+        // 从第i=0列开始给我遍历！！！
+        for (int i=0; i<n; i++){
+            if (isOK(solution, row, i)){
+                solution[row] = i;  // 放一个Queen
+                backtrack2(n, row+1, solution, solutions);
+            }
+            // 如果遍历完一行都没有合适的会退出递归，返回上一级
+        }
 
-        return getSolutionBoard(solutions);
+    }
+
+    public boolean isOK(int[] solution, int row, int col){
+        for (int i=0; i<row; i++){
+            if (col == solution[i]){
+                return false;
+            }
+            if (col-solution[i]==row-i || col-solution[i]==i-row){
+                return false;
+            }
+        }
+        return true;
     }
 
     // 构建题目需要的类型
-    public List<List<String>> getSolutionBoard(List<int[]> solutions){
-        // 所有解
-        List<List<String>> solutionBoard = new ArrayList<List<String>>();
-        for (int[] solution : solutions) {
-            // 每个解
-            ArrayList<String> ans = new ArrayList<String>();
-            for (int i : solution) {
-                // 一个解的每一行
-                StringBuilder row = new StringBuilder();
-                for (int j=0; j<solution.length; j++){
-                    if (i!=j){
-                        row.append('.');
-                    }else{
-                        row.append('Q');
-                    }
+    public List<String> getSolutionBoard(int[] solution){
+        // 每个解
+        ArrayList<String> ans = new ArrayList<String>();
+        for (int i : solution) {
+            // 一个解的每一行
+            StringBuilder row = new StringBuilder();
+            for (int j=0; j<solution.length; j++){
+                if (i!=j){
+                    row.append('.');
+                }else{
+                    row.append('Q');
                 }
-                ans.add(row.toString());
             }
-            solutionBoard.add(ans);
+            ans.add(row.toString());
         }
-
-        return solutionBoard;
+        return ans;
     }
-
-
-
-
 
 }
