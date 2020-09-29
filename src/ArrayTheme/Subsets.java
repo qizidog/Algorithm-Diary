f23e88389d99a1fa076f2787481577e9be168d5d
qizidog <451559450q@gmail.com>
2020-09-29 13:25:05 +0800

    2020.9.29 Subsets2 居然被这道题搞疯了

--- src/ArrayTheme/Subsets.java
+++ src/ArrayTheme/Subsets.java
@@ -1,4 +1,4 @@
-package BackTracking;
+package ArrayTheme;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,4 +41,44 @@ public class Subsets {
             }
         }
     }
+
+
+    // 官方回溯解法：难以理解
+    List<Integer> t = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> subsets2(int[] nums) {
+        dfs2(0, nums);
+        return ans;
+    }
+
+    public void dfs2(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<Integer>(t));
+            return;
+        }
+        t.add(nums[cur]);
+        dfs2(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs2(cur + 1, nums);
+    }
+
+
+    // 官方迭代法：涉及到位运算，需要研究下
+    // List<Integer> t = new ArrayList<Integer>();
+    // List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    public List<List<Integer>> subsets3(int[] nums) {
+        int n = nums.length;
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            for (int i = 0; i < n; ++i) {
+                if ((mask & (1 << i)) != 0) {
+                    t.add(nums[i]);
+                }
+            }
+            ans.add(new ArrayList<Integer>(t));
+        }
+        return ans;
+    }
+
 }
---
+++ src/ArrayTheme/Subsets2.java
@@ -0,0 +1,61 @@
+package ArrayTheme;
+
+import java.util.*;
+
+/**
+ * @author : qizidog
+ * @date : 2020-09-29 11:00
+ * @description :
+ **/
+
+public class Subsets2 {
+    /*// 这种方法搞不定了
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> temp = new ArrayList<Integer>();
+
+        for (int i=0; i<=nums.length; i++) {
+            backtracking(nums, ans, temp, i, 0);
+        }
+
+        return ans;
+    }
+
+    public void backtracking(int[] nums, List<List<Integer>> ans, List<Integer> temp,
+                             int targetNums, int startNum){
+        if (temp.size()==targetNums){
+            ans.add(new ArrayList<Integer>(temp));
+        }else{
+            for (int i=startNum; i<nums.length; i++) {
+                if (i == 0 || nums[i-1]!=nums[i]) {
+                    temp.add(nums[i]);
+                    backtracking(nums, ans, temp, targetNums, startNum + 1);
+                    temp.remove(temp.size() - 1);
+                }
+            }
+        }
+    }*/
+
+
+    // 官方解法
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums); //排序
+        getAns(nums, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void getAns(int[] nums, int start, ArrayList<Integer> temp, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(temp));
+        for (int i = start; i < nums.length; i++) {
+            //和上个数字相等就跳过
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            temp.add(nums[i]);
+            getAns(nums, i + 1, temp, ans);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
