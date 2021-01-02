06a395ad7912dcd8b1df2b4f73b775a0375a5a0f
qizidog <451559450q@gmail.com>
2021-01-02 23:17:40 +0800

    "2021.1.2 手写单链表和双链表，并且实现链表的反转"

---
+++ src/LinkedListTheme/ListReverse.java
@@ -0,0 +1,152 @@
+package LinkedListTheme;
+
+/**
+ * @author : qizidog
+ * @date : 2021-01-02 22:11
+ * @description :
+ * 实现单链表或双链表的反转（基础）
+ **/
+
+public class ListReverse {
+
+    /*
+        单链表节点
+     */
+    static class SingleListNode<E>{
+        SingleListNode<E> next;
+        E value;
+
+        public SingleListNode(E value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "SingleListNode{" +
+                    "value=" + value +
+                    ", next=" + (next==null?"null":next.value) +
+                    '}';
+        }
+    }
+
+    /*
+        双链表节点
+     */
+    static class DoubleListNode<E>{
+        DoubleListNode<E> next;
+        DoubleListNode<E> pre;
+        E value;
+
+        public DoubleListNode(E value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "DoubleListNode{" +
+                    "pre=" + (pre==null?"null":pre.value) +
+                    ", value=" + value +
+                    ", next=" + (next==null?"null":next.value) +
+                    '}';
+        }
+    }
+
+
+    /*
+        创建一个单链表
+     */
+    public static SingleListNode<Integer> createSingleList(){
+        SingleListNode<Integer> head = new SingleListNode<>(0);
+        SingleListNode<Integer> pointer = head;
+        for (int i=1; i<6; i++){
+            SingleListNode<Integer> cur = new SingleListNode<>(i);
+            pointer.next = cur;
+            pointer = cur;
+        }
+        return head;
+    }
+
+    /*
+        创建一个双链表
+     */
+    public static DoubleListNode<Integer> createDoubleList(){
+        DoubleListNode<Integer> head = new DoubleListNode<>(0);
+        DoubleListNode<Integer> pointer = head;
+        // DoubleListNode<Integer> pre = null;
+        for (int i=1; i<6; i++){
+            DoubleListNode<Integer> cur = new DoubleListNode<Integer>(i);
+            pointer.next = cur;
+            cur.pre = pointer;
+            pointer = cur;
+        }
+        return head;
+    }
+
+    /*
+        单链表反转
+     */
+    public static SingleListNode reverseList(SingleListNode head){
+        SingleListNode pre = null;
+        SingleListNode next = null;
+
+        while(head!=null){
+            next = head.next;
+            head.next = pre;
+
+            // 为下一次循环做准备
+            pre = head;
+            head=next;
+        }
+        return pre;
+    }
+
+    /*
+        双链表反转
+     */
+    public static DoubleListNode reverseList(DoubleListNode head){
+        DoubleListNode pre = null;
+        DoubleListNode next = null;
+
+        while(head!=null){
+            next = head.next;
+            pre = head.pre;
+
+            head.next = pre;
+            head.pre = next;
+
+            pre = head;
+            head = next;
+        }
+
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        // 单链表反转测试
+        SingleListNode<Integer> singleList = createSingleList();
+
+        singleList = reverseList(singleList);
+
+        // 查看链表
+        SingleListNode<Integer> singlePointer = singleList;
+        for (int i=0; i<6; i++){
+            System.out.println(singlePointer);
+            singlePointer = singlePointer.next;
+        }
+
+        System.out.println("==============================");
+
+        // 双链表反转测试
+        DoubleListNode<Integer> doubleList = createDoubleList();
+
+        doubleList = reverseList(doubleList);
+
+        // 查看链表
+        DoubleListNode<Integer> doublePointer = doubleList;
+        for (int i=0; i<6; i++){
+            System.out.println(doublePointer);
+            doublePointer = doublePointer.next;
+        }
+
+    }
+}
